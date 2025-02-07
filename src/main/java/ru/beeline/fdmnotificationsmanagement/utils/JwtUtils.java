b0c620019a20397227a4803d0e1297ce2366b42b
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2025-02-07 14:39:27 +0300

    SFDM-1774 Перейти на новый тип авторизации в rabbitMQ

---
+++ src/main/java/ru/beeline/fdmnotificationsmanagement/utils/JwtUtils.java
@@ -0,0 +1,35 @@
+package ru.beeline.fdmnotificationsmanagement.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONObject;
+
+import java.util.Base64;
+import java.util.Map;
+
+
+@Slf4j
+public class JwtUtils {
+    public static Map<String, Object> encodeJWT(String token) {
+        try {
+            String[] parts = token.split("\\.");
+            Base64.Decoder decoder = Base64.getUrlDecoder();
+
+            String payload = new String(decoder.decode(parts[1]));
+            String jsonObject = JSONObject.escape(payload).replace("\\", "");
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            // convert JSON string to Map
+            Map<String, Object> map = mapper.readValue(jsonObject, Map.class);
+
+            return map;
+
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
+        return null;
+    }
+
+}
