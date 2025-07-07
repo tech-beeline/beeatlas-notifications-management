5081ff05e23c4c9e37b61f7f5c6935dad52e693a
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2025-07-07 14:15:53 +0300

    fix swagger

--- src/main/java/ru/beeline/fdmnotificationsmanagement/config/SwaggerConfig.java
+++ src/main/java/ru/beeline/fdmnotificationsmanagement/config/SwaggerConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -18,15 +19,31 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(getApiInfo())
+                .securitySchemes(List.of(apiKey()))
+                .securityContexts(List.of(securityContext()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("ru.beeline.fdmnotificationsmanagement.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
 
+    private ApiKey apiKey() {
+        return new ApiKey("Bearer", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{authorizationScope};
+        return List.of(new SecurityReference("Bearer", authorizationScopes));
+    }
+
     private ApiInfo getApiInfo() {
         return new ApiInfo(
-                "Eafdmmart API",
+                "Notifications management API",
                 "API документация",
                 "1.0",
                 "Terms of service",
